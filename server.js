a7dd114d55b128e8a56ea040227a8dbbb02b2f86
DarrylAdigwu <115912161+DarrylAdigwu@users.noreply.github.com>
2025-02-26 23:31:37 -0800

    added env files

--- server.js
+++ server.js
@@ -13,14 +13,33 @@ server.set("view engine", "ejs")
 // Configure .env files
 dotenv.config();
 
-// Web security
+// Middleware for web security
 server.use(helmet());
 
-// Cross origin resources
+// Middleware for cross-origin resources
 server.use(cors());
 
-// Configure middleware for JSON, public folder, and
+// Configure middleware for JSON, public folder, and parsing body
 server.use(express.static("public"));
 server.use(express.json());
 server.use(express.urlencoded({extended:true}))
 
+// Global Error Handling
+server.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something unusual occurred");
+});
+
+// Log http request
+server.use(morgan("tiny"));
+
+
+// Landing page
+server.route("/")
+.get(async (req, res) => {
+
+});
+
+server.listen(process.env.PORT, () => {
+  console.log(`Server listening on port ${process.env.PORT}`);
+});
